992b449e34413ae529cf1db3839a07305e74c2c0
Derek03 <derek.braden.larson@gmail.com>
2016-11-25 14:22:09 -0800

    fixed bug with getting individual item

--- application/controllers/Maintenance.php
+++ application/controllers/Maintenance.php
@@ -31,6 +31,17 @@ class maintenance extends Rest_Controller {
       }
   }
 
+	// Handle an incoming GET ... return 1 menu item
+	function item_get()
+	{
+	    $key = $this->get('id');
+	    $result = $this->menu->get($key);
+	    if ($result != null)
+	        $this->response($result, 200);
+	    else
+	        $this->response(array('error' => 'Menu item not found!'), 404);
+	}
+
   // Handle an incoming POST - add a new menu item
   function index_post()
   {
